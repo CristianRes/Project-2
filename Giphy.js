df915b1730d26b8c0eebe8e9f434974162e5c7cf
CristianRes <cristianrez07@gmail.com>
2025-09-06 01:31:13 -0500

    Commit 4

--- Giphy.js
+++ Giphy.js
@@ -1,11 +1,11 @@
-
 $(document).ready(()=>{
 
     $("#submit").click(()=>{
         let userInput = $("#search").val()
         alert(userInput)
-    })
         $.ajax({
-            url:"api.giphy.com/v1/gifs/search/?apikey=pCR0lOkznMC9UJnexnpr7moSijsIX80w&"
-    })
+            url:'api.giphy.com/v1/gifs/search?apiKey=pCR0lOkznMC9UJnexnpr7moSijsIX80w'
+        })
+        })
+        
 })
