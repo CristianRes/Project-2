1acdcafd80a28116a22c9433c27469b4cb0e4f86
CristianRes <cristianrez07@gmail.com>
2025-09-07 23:14:06 -0500

    Commit 9

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ $(document).ready(()=>{
         $("submit").click(()=>{
             let userInput = $('#search').val()
             alert(userInput)
-
-        })
-        $.ajax({
+            $.ajax({
             url:'https://api.giphy.com/v1/gifs/search?api_key={pCR0lOkznMC9UJnexnpr7moSijsIX80w}'
     })
+        })
+        
+})
